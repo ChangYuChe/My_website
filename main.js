aa36e0d3632cfcf756af1a4bfccef8c4529fce4f
CHANG YU CHE <39546760+ChangYuChe@users.noreply.github.com>
2020-12-23 15:25:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function showMobileMenu(){
+    let mobilemenu=document.querySelector("#mobile-menu");
+    mobilemenu.style.display="block";
+}
+function closeMobileMenu(){
+    let mobilemenu=document.querySelector("#mobile-menu");
+    mobilemenu.style.display="none";
+}
+function OpacityUnset3(){
+    let picture3=document.querySelector("#picture3");
+    picture3.style.opacity="unset";
+    picture3.style.transition="opacity 0.5s";
+}
+function OpacitySet3(){
+    let picture3=document.querySelector("#picture3");
+    picture3.style.opacity="0.8";
+}
+function OpacityUnset4(){
+    let picture4=document.querySelector("#picture4");
+    picture4.style.opacity="unset";
+    picture4.style.transition="opacity 0.5s";
+}
+function OpacitySet4(){
+    let picture4=document.querySelector("#picture4");
+    picture4.style.opacity="0.8";
+}
